37db8143988f8efdb3859d02e2442068a5f7ea75
d-otis <dan@dan-foley.com>
2020-06-01 18:23:42 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,25 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  accepts_nested_attributes_for :categories, reject_if: :all_blank
+  # accepts_nested_attributes_for :categories, reject_if: :all_blank
+
+  def categories_attributes=(categories_attributes)
+  	if !trash_hash?(categories_attributes)
+  		categories_attributes.values.each do |category_attribute|
+  			category = Category.find_or_create_by(category_attribute)
+  			self.post_categories.build(category: category)
+  		end
+  	end
+  end
+
+  def unique_users
+    self.users.uniq
+  end
+
+  private
+
+  def trash_hash?(hsh)
+  	hsh.values.first.has_value?("")
+  end
 
 end
